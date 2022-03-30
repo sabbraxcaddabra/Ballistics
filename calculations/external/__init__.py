d2c9b79807446be75d2d7ff4950c619a22304d76
sabbraxcaddabra <sabbraxcaddabra@yandex.ru>
2022-03-30 15:52:00 +0300

    Поправлена структура пакета

--- calculations/external/__init__.py
+++
@@ -1,4 +0,0 @@
-from Ballistics.calculations.external_ballistics import count_eb
-from Ballistics.calculations.internal_ballistics import count_ib
-
-__all__ = ['count_eb', 'count_ib']
--- calculations/optimize/__init__.py
+++
@@ -1,4 +0,0 @@
-from .external_ballistics import count_eb
-from .internal_ballistics import count_ib
-
-__all__ = ['count_eb', 'count_ib']
